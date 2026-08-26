9e7b9b77ad21dea55938487ccb2f08d089883d34
bawher12 <60037556+bawher12@users.noreply.github.com>
2026-08-26 05:29:52 -0500

    Update library-render.js

--- js/library-render.js
+++ js/library-render.js
@@ -46,7 +46,7 @@ function cardHtml(item) {
     listEl.innerHTML = catalog.map(cardHtml).join("");
   }
 
-  fetch("data/library.json", { cache: "no-store" })
+  fetch("data/library.json?v=" + Date.now(), { cache: "no-store" })
     .then((res) => res.json())
     .then((data) => {
       catalog = data;
